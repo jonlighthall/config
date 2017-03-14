32fc6c7e306c8c0a649fbb101e52b6607c7e6019
Jon Lighthall <jon.lighthall@gmail.com>
2017-03-14 16:12:16 -0400

    Add headings in comments
    Add section heading in comments and rearrange settings under headings.

--- cygwin/.emacs.d/init.el
+++ cygwin/.emacs.d/init.el
@@ -1,3 +1,4 @@
+;;; Settings for DOS and Cygwin 
 (setq exec-path '("C:/cygwin/bin/"))
 (setq-default ispell-program-name "aspell")
 
@@ -14,34 +15,39 @@
   (setq buffer-display-table (make-display-table))
   (aset buffer-display-table ?\^M []))
 
+;;; Spelling
 (dolist (hook '(text-mode-hook))
-(add-hook hook (lambda () (flyspell-mode 1))))
+  (add-hook hook (lambda () (flyspell-mode 1))))
 (add-hook 'c++-mode-hook
-(lambda ()
-(flyspell-prog-mode)
-))
+	  (lambda ()
+	    (flyspell-prog-mode)
+	    ))
 (add-hook 'LaTeX-mode-hook 'flyspell-mode)
 
 ;; Program-mode spell checking for most program languages
 (mapcar (lambda (mode-hook) (add-hook mode-hook 'flyspell-prog-mode))
         '(c-mode-common-hook tcl-mode-hook emacs-lisp-mode-hook 
-          ruby-mode-hook java-mode-hook))
+			     ruby-mode-hook java-mode-hook))
 
-;;(add-hook 'flyspell-mode-hook 'flyspell-buffer) ;; slows down loading
+;;(add-hook 'flyspell-mode-hook 'flyspell-buffer) ;; slows downloading
 
+;;; Frame appearance and behavior
 ;; Set frame size
 (set-frame-size (selected-frame) 99 39);; (columns,rows)
-;; "snap" width is 73 (for 1280 px wide display)
-;; "pretty-print" width is 97 columns wide (+2 for line numbers)
-;; max height with triple-hieght taskbar is 40 (1366 res.)
+					; "snap" width is 73 (for 1280 px wide display)
+					; "pretty-print" width is 97 columns wide (+2 for line numbers)
+					; max height with triple-hieght taskbar is 40 (1366 res.)
 
 ;; Set frame position
-(setq initial-frame-alist '((top . 0) (left . 0)))
-;; moves window to upper left corner
+(setq initial-frame-alist '((top . 0) (left . 0))) ; moves window to upper left corner
 
 ;; default to better frame titles
 (setq frame-title-format
       (concat  "%b - emacs@" (system-name)))
+
+;; Window transparency
+(set-frame-parameter (selected-frame) 'alpha '(95 . 95))
+;;(add-to-list 'default-frame-alist '(alpha . (95 . 95)))
 
 (custom-set-variables
  ;; Split ediff window vertically
@@ -49,6 +55,7 @@
  ;; Turn off welcome page
  '(inhibit-startup-screen t))
 
+;;; Highlighting and coding aides
 ;; Syntax highlighting (font-lock-mode)
 (cond ((fboundp 'global-font-lock-mode)
        ;; Turn on font-lock in all modes that support it
@@ -64,10 +71,6 @@
 (global-linum-mode 1)
 ;;(setq linum-format "%d ")
 
-;; Custom keyboard shortcuts
+;;; Custom keyboard shortcuts
 (global-set-key (kbd "C-x e") 'ediff-buffers) 
 (global-set-key (kbd "C-x w") 'ediff-revision)  
-
-;; Window transparency
-(set-frame-parameter (selected-frame) 'alpha '(95 . 95))
-;(add-to-list 'default-frame-alist '(alpha . (95 . 95)))
