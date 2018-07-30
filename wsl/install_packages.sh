71c3c2f37258e89dab85b6a6f2e59d694b052944
Jon Lighthall <jon.lighthall@gmail.com>
2018-07-30 14:08:11 -0400

    Update install_packages.sh

--- wsl/install_packages.sh
+++ wsl/install_packages.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-sudo apt install emacs
+sudo apt-get install emacs
+sudo apt install aspell
 
-sudo apt-get install -y dbus
+#sudo apt-get install -y dbus
 #sudo dbus-uuidgen > /var/lib/dbus/machine-id
-sudo dbus-uuidgen > machine-id
-sudo mv machine-id /var/lib/dbus/
+#dbus-uuidgen > machine-id
+#sudo mv machine-id /var/lib/dbus/
 sudo apt-get install dbus-x11
-
-sudo apt-get update
-sudo apt-get upgrade
 
 sudo apt-get install x11-apps
 export LC_ALL=C
@@ -19,10 +17,7 @@ xcalc &
 xman &
 xlogo &
 
-
 sudo apt install nautilus
-sudo apt install diff
-sudo apt install diffutils
-git config --global push.default simple
 
-sudo apt install aspell
+sudo apt-get update
+sudo apt-get upgrade --fix-missing
