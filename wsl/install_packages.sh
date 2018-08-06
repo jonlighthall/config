c9718ef16a6e44cb5b97b1e156b899cfcf8fadc6
Jon Lighthall <jon.lighthall@gmail.com>
2018-08-06 13:18:50 -0400

    update apt-get to apt, remove unneeded dbus apps

--- wsl/install_packages.sh
+++ wsl/install_packages.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-sudo apt-get install emacs
+sudo apt install emacs
 sudo apt install aspell
+sudo apt install dbus-x11
 
-#sudo apt-get install -y dbus
-#sudo dbus-uuidgen > /var/lib/dbus/machine-id
-#dbus-uuidgen > machine-id
-#sudo mv machine-id /var/lib/dbus/
-sudo apt-get install dbus-x11
-
-sudo apt-get install x11-apps
+sudo apt install x11-apps
 export LC_ALL=C
 xeyes &
 xclock &
@@ -19,5 +14,5 @@ xlogo &
 
 sudo apt install nautilus
 
-sudo apt-get update
-sudo apt-get upgrade --fix-missing
+sudo apt update
+sudo apt upgrade --fix-missing
