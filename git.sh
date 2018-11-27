ac148b968fd1ce04993f0d26f9f5cbe449b8e5cd
Jon Lighthall <jon.lighthall@gmail.com>
2018-11-27 10:05:30 -0600

    change permissions and add git settings

--- git.sh
+++ git.sh
@@ -3,3 +3,6 @@
 echo "setting Git user..."
 git config --global user.name "Jon Lighthall"
 git config --global user.email "jon.lighthall@gmail.com"
+
+echo "setting Git preferences..."
+git config --global color.ui true
