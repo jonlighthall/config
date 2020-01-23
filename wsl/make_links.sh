0bc1e61d9c1459564c2405ba0c43f6fc41be8e6f
Jon Lighthall <jon.lighthall@gmail.com>
2020-01-23 14:27:32 -0600

    split personal scripts out from make links

--- wsl/make_links.sh
+++ wsl/make_links.sh
@@ -8,7 +8,6 @@ fi
 
 ln -s ~/config/wsl/.bash_profile ~/.bash_profile
 
-
 if [ -d ~/.emacs.d ]; then
     echo Backing up .emacs.d
     mv ~/.emacs.d/ ~/.emacs.d_old/
@@ -20,25 +19,5 @@ if [ -f ~/.gitconfig ]; then
     mv ~/.gitconfig ~/.gitconfig_old
 fi
 ln -s ~/config/wsl/.gitconfig ~/.gitconfig
-ln -s /mnt/c/Users/jonli/OneDrive/Documents/.cygwin_home/.git-credentials ~/.git-credentials
 
 ln -s ~/config/wsl/.rootrc ~/.rootrc
-
-## Copy and link from OneDrive
-# Bash history
-if [ -f ~/.bash_history ]; then
-cat ~/.bash_history >> /mnt/c/Users/jonli/OneDrive/Documents/.cygwin_home/.bash_history
-rm ~/.bash_history
-fi
-ln -s /mnt/c/Users/jonli/OneDrive/Documents/.cygwin_home/.bash_history ~/.bash_history
-
-# Copy .ssh
-if [ -d ~/.ssh ]; then
-    echo Backing up .ssh
-    mv -rv ~/.ssh/ ~/.ssh_old
-fi
-git clone https://jonlighthall@bitbucket.org/jonlighthall/.ssh.git ~/.ssh
-chmod 600 ~/.ssh/config 
-chmod 600 ~/.ssh/id_rsa
-
-ln -s /mnt/c/Users/jonli/OneDrive/ ~/onedrive
