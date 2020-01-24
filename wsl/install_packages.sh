5e69f27d8e109250618b54778389c743693db8d0
Jon Lighthall <jon.lighthall@gmail.com>
2020-01-24 14:27:35 -0600

    add update to begining, add remove to end, add comments

--- wsl/install_packages.sh
+++ wsl/install_packages.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+# update and upgrade
+sudo apt update
+sudo apt upgrade -y
+
+# install packages
 sudo apt install -y emacs
 sudo apt install -y aspell
-sudo apt install -y dbus-x11
 
+# install and test X11
+sudo apt install -y dbus-x11
 sudo apt install -y x11-apps
 export LC_ALL=C
 xeyes &
@@ -12,7 +18,9 @@ xcalc &
 xman &
 xlogo &
 
+# install more packages
 sudo apt install -y nautilus
 
-sudo apt update
+# re-check and cleanup
 sudo apt upgrade -y --fix-missing
+sudo apt autoremove -y
