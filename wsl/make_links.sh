48b19d9bee88e6185add25c5d23ce8cebbc92ae9
Jon Lighthall <jon.lighthall@gmail.com>
2021-01-25 15:42:40 -0600

    unify make links, add dir var; Oct 15, 2020

--- wsl/make_links.sh
+++ wsl/make_links.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
+SRCDIR=~/config/wsl
 
 ## Link from config repo
 if [ -f ~/.bash_profile ]; then
     echo Backing up .bash_profile
     mv ~/.bash_profile ~/.bash_profile_old 
 fi
-ln -s ~/config/wsl/.bash_profile ~/.bash_profile
+ln -s ${SRCDIR}/.bash_profile ~/.bash_profile
 
 if [ -d ~/.emacs.d ]; then
     echo Backing up .emacs.d
     mv ~/.emacs.d/ ~/.emacs.d_old/
 fi
-ln -s ~/config/wsl/.emacs.d/ ~/.emacs.d
+ln -s ${SRCDIR}/.emacs.d/ ~/.emacs.d
 
 if [ -f ~/.gitconfig ]; then
     echo Backing up .gitconfig
     mv ~/.gitconfig ~/.gitconfig_old
 fi
-ln -s ~/config/wsl/.gitconfig ~/.gitconfig
+ln -s ${SRCDIR}/.gitconfig ~/.gitconfig
 
 if [ -f ~/.rootrc ]; then
     echo Backing up .rootrc
     mv ~/.rootrc ~/.rootrc_old
 fi
-ln -s ~/config/wsl/.rootrc ~/.rootrc
+ln -s ${SRCDIR}/.rootrc ~/.rootrc
