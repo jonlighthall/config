1c626c12d42a640031554ab5d10d8d9beb8f4b2b
Jon Lighthall <jon.lighthall@gmail.com>
2021-08-20 13:40:26 -0500

    FIX: remove mv =r, add tgtdir

--- wsl/make_links_personal.sh
+++ wsl/make_links_personal.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
+TGTDIR=$HOME
 
 ## Link from outside of repo
-ln -s /mnt/c/Users/jonli/OneDrive/Documents/.cygwin_home/.git-credentials ~/.git-credentials
+my_link=.git-credentials
+if [ -e $TGTDIR/${my_link} ]
+   echo " Backing up ${my_link}..."
+   mv -v $TGTDIR/${my_link} $TGTDIR/${my_link}_$(date +'%Y-%m-%d-t%H%M')
+fi
+ln -s /mnt/c/Users/jonli/OneDrive/Documents/.cygwin_home/${my_link} $TGTDIR/${my_link}
 
 ## Copy and link from OneDrive
 # Bash history
-if [ -f ~/.bash_history ]; then
-cat ~/.bash_history >> /mnt/c/Users/jonli/OneDrive/Documents/.cygwin_home/.bash_history
-rm ~/.bash_history
+if [ -f $TGTDIR/.bash_history ]; then
+cat $TGTDIR/.bash_history >> /mnt/c/Users/jonli/OneDrive/Documents/.cygwin_home/.bash_history
+rm $TGTDIR/.bash_history
 fi
 ln -s /mnt/c/Users/jonli/OneDrive/Documents/.cygwin_home/.bash_history ~/.bash_history
 
 # Copy .ssh
-if [ -d ~/.ssh ]; then
+if [ -d $TGTDIR/.ssh ]; then
     echo Backing up .ssh
-    mv -rv ~/.ssh/ ~/.ssh_old
+    mv -v $TGTDIR/.ssh/ ~/.ssh_old
 fi
 git clone https://jonlighthall@bitbucket.org/jonlighthall/.ssh.git ~/.ssh
-chmod 600 ~/.ssh/config 
-chmod 600 ~/.ssh/id_rsa
+chmod 600 $TGTDIR/.ssh/config 
+chmod 600 $TGTDIR/.ssh/id_rsa
 
-ln -s /mnt/c/Users/jonli/OneDrive/ ~/onedrive
+ln -sv /mnt/c/Users/jonli/OneDrive/ $TGTDIR/onedrive
