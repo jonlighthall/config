5040cf63e1d9c160da85b5ea449ce07c0f60b298
Jon Lighthall <jon.lighthall@gmail.com>
2021-08-20 14:15:14 -0500

    add more checks and add winhome

--- wsl/make_links_personal.sh
+++ wsl/make_links_personal.sh
@@ -3,27 +3,37 @@ TGTDIR=$HOME
 
 ## Link from outside of repo
 my_link=.git-credentials
-if [ -e $TGTDIR/${my_link} ]
-   echo " Backing up ${my_link}..."
-   mv -v $TGTDIR/${my_link} $TGTDIR/${my_link}_$(date +'%Y-%m-%d-t%H%M')
+if [ -f $TGTDIR/${my_link} ] || [ -L $TGTDIR/${my_link} ]; then
+    echo " Backing up ${my_link}..."
+    mv -v $TGTDIR/${my_link} $TGTDIR/${my_link}_$(date +'%Y-%m-%d-t%H%M')
 fi
-ln -s /mnt/c/Users/jonli/OneDrive/Documents/.cygwin_home/${my_link} $TGTDIR/${my_link}
+ln -sv /mnt/c/Users/jonli/OneDrive/Documents/.cygwin_home/${my_link} $TGTDIR/${my_link}
 
 ## Copy and link from OneDrive
 # Bash history
 if [ -f $TGTDIR/.bash_history ]; then
-cat $TGTDIR/.bash_history >> /mnt/c/Users/jonli/OneDrive/Documents/.cygwin_home/.bash_history
-rm $TGTDIR/.bash_history
+    echo "backing up .bash_history..."
+    if [ ! -L $TGTDIR/.bash_history ]; then # only copy if not already a link
+	cat $TGTDIR/.bash_history >> /mnt/c/Users/jonli/OneDrive/Documents/.cygwin_home/.bash_history
+    else
+	echo "$TGTDIR/.bash_history is already a link"
+    fi
+    mv $TGTDIR/.bash_history$TGTDIR/.bash_history_$(date +'%Y-%m-%d-t%H%M')
 fi
-ln -s /mnt/c/Users/jonli/OneDrive/Documents/.cygwin_home/.bash_history ~/.bash_history
+ln -sv /mnt/c/Users/jonli/OneDrive/Documents/.cygwin_home/.bash_history $TGTDIR/.bash_history
 
 # Copy .ssh
 if [ -d $TGTDIR/.ssh ]; then
-    echo Backing up .ssh
-    mv -v $TGTDIR/.ssh/ ~/.ssh_old
+    echo "Backing up .ssh..."
+    mv -v $TGTDIR/.ssh/ ~/.ssh_$(date +'%Y-%m-%d-t%H%M')
 fi
 git clone https://jonlighthall@bitbucket.org/jonlighthall/.ssh.git ~/.ssh
 chmod 600 $TGTDIR/.ssh/config 
 chmod 600 $TGTDIR/.ssh/id_rsa
 
-ln -sv /mnt/c/Users/jonli/OneDrive/ $TGTDIR/onedrive
+if [ ! -e $TGTDIR/winhome ]; the
+   ln -sv /mnt/c/Users/jonli $TGTDIR/winhome
+fi
+   if [ ! -e $TGTDIR/winhome ]; the
+      ln -sv /mnt/c/Users/jonli/OneDrive/ $TGTDIR/onedrive
+   fi
