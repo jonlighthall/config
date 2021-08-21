32021eda758e04834b62804f0c5ba709e8ccf300
Jon Lighthall <jon.lighthall@gmail.com>
2021-08-21 12:14:08 -0500

    add vars and format

--- windows/make_links.ps1
+++ windows/make_links.ps1
@@ -10,32 +10,29 @@
 $local = 'C:\Users\jonli\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt'
 $cloud = 'C:\Users\jonli\OneDrive\Documents\ConsoleHost_history.txt'
 
-echo "test local history...`n$local"
+echo "test local history..."
 if (Test-Path -Path $local) {
-   echo "found"
-   echo "test cloud history...`n$cloud"
-   if (Test-Path -Path $cloud) {
-      echo "found"
-      echo "proceeding with append, delete, and link..."
-
-#if
-    echo "appending local copy with synced copy..."
-#    type C:\Users\jonli\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt >> C:\Users\jonli\OneDrive\Documents\ConsoleHost_history.txt
-    echo "removing local copy"
-#    del C:\Users\jonli\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt
-#else
-    echo "local copy not found"
-#fi
-echo "creating symbolic link"
-#mklink C:\Users\jonli\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt C:\Users\jonli\OneDrive\Documents\ConsoleHost_history.txt
-
-
-   }    
-   else {
-      echo "not found"
-      echo "no 
-   }
-   }
-   else {
-   echo "not found"
-   }
+       echo "$local found"
+       echo "test cloud history..."
+       if (Test-Path -Path $cloud) {
+	      echo "$cloud found"
+	      echo "proceeding with append, delete, and link..."
+	      
+	      #if
+	      echo "appending local copy with cloud copy..."
+	      #    type $local >> $cloud
+	      echo "removing local copy"
+	      #    del $local
+	      #fi
+	      echo "creating symbolic link"
+	      #mklink $local $cloud
+	      
+	}    
+	else {
+	      echo "$cloud not found"
+  	      echo "no history to link to`nexiting"
+        }	   
+}	   
+else {  
+echo "$local not found"
+}
