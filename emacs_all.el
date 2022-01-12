3ec0cfe0ab5fe2921f1210bfa6aac388c789a589
Jon Lighthall <jon.lighthall@gmail.com>
2022-01-12 11:51:13 -0600

    add bash script association

--- emacs_all.el
+++ emacs_all.el
@@ -50,6 +50,9 @@
 ;; setup files containing “makefile” to open in makefile-mode
 (add-to-list 'auto-mode-alist '("[Mm]akefile.+\\'" . makefile-mode))
 
+;; files containing “bash” to open in shell-script-mode
+(add-to-list 'auto-mode-alist '(".bash.+\\'" . shell-script-mode))
+
 ;; turn off starup message
 (defun display-startup-echo-area-message ()
   (message nil))
