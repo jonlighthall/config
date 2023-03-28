4cc42fb09f747b1e7319bab4bf13ee195226a26f
Jon Lighthall <jon.lighthall@gmail.com>
2023-03-28 10:58:10 -0500

    add sort words

--- emacs_all.el
+++ emacs_all.el
@@ -100,6 +100,18 @@
   )
 (global-set-key (kbd "C-x y") 'sort-bash-history)
 
+;; sort words in region
+(defun sort-words (reverse beg end)
+  "Sort words in region alphabetically, in REVERSE if negative.
+    Prefixed with negative \\[universal-argument], sorts in reverse.
+
+    The variable `sort-fold-case' determines whether alphabetic case
+    affects the sort order.
+
+    See `sort-regexp-fields'."
+  (interactive "*P\nr")
+  (sort-regexp-fields reverse "\\w+" "\\&" beg end))
+
 ;; start Git merge conflicts in smerge ediff
 (defun vc-git-find-file-hook ()
   (when (save-excursion
