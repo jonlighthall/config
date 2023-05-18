93cc75d24106f71d4db1e4dd3a82620472ad0cac
Jon Lighthall <jon.lighthall@gmail.com>
2023-05-18 13:41:38 -0500

    FIX dir location, add tabs

--- get_repos.sh
+++ get_repos.sh
@@ -6,27 +6,29 @@ if [ ! "$BASH_SOURCE" = "$src_name" ]; then
     echo -n " -> $src_name"
 fi
 echo "..."
+TAB='   '
 if [ $# -eq 0 ]; then
     echo "No system specified"
 fi
 if [ $# -eq 1 ]; then
-    echo "Configuring options for $1"
-    echo -n "$1... "
+    echo "Loading configuration options for $1"
+    echo -n "${TAB}$1... "
     if [ -d $1 ]; then
 	echo "found"
 	cd $1
 	fname=make_links.sh
-	echo -n "$fname... "
+	echo -n "${TAB}$fname... "
 	if [ -f $fname ]; then
 	    echo "found"
 	    ./$fname
 	    src_name2=~/.bash_profile
-	    echo "$src_name2... "
+	    echo -n "$src_name2... "
 	    if [ -f $src_name2 ]; then
 		echo "found"
 		source $src_name2
 		echo
 		echo "configuration applied for $1"
+		echo
 	    else
 		echo "not found"
 	    fi
@@ -38,10 +40,12 @@ if [ $# -eq 1 ]; then
     fi
 fi
 
-rdir=repos
-udir=utils
-edir=examp
+# define directory names
+rdir=${HOME}/repos
+udir=${HOME}/utils
+edir=${HOME}/examp
 
+echo "creating repository directories..."
 for my_dir in $rdir $udir $edir
 do
     mkdir -vp ${my_dir}
@@ -55,18 +59,18 @@ github_user=jonlighthall
 github_https=https://github.com/${github_user}/
 github_ssh=git@github.com:${github_user}/
 
-cd ${HOME}/${rdir}
+cd ${rdir}
 # list of repos to be cloned
 for my_repo in bash batch fortran_utilities powershell
 do
     git clone ${github_https}${my_repo}
-    ln -sv ${HOME}/${udir}/${my_repo} ${HOME}/${rdir}/${my_repo}
+    ln -sv ${udir}/${my_repo} ${rdir}/${my_repo}
 done
 
 for my_repo in fortran hello nrf python
 do
     git clone ${github_https}$my_repo
-    ln -sv ${HOME}/${edir}/${my_repo} ${HOME}/${rdir}/${my_repo}
+    ln -sv ${edir}/${my_repo} ${rdir}/${my_repo}
 done
 
 
