b373743a959e0f8a61f7cfd74a6d80db83e4d451
Jon Lighthall <jon.lighthall@gmail.com>
2023-05-18 14:27:12 -0500

    pull formatting updates from other repo

--- wsl/make_links.sh
+++ wsl/make_links.sh
@@ -7,7 +7,10 @@ if [ ! "$BASH_SOURCE" = "$src_name" ]; then
 fi
 echo "..."
 
-TAB="   "
+fpretty=${HOME}/utils/bash/.bashrc_pretty
+if [ -e $fpretty ]; then
+    source $fpretty
+fi
 
 # set source and target directories
 source_dir=$(dirname $src_name)
@@ -35,9 +38,7 @@ else
     fi
 fi
 
-echo "--------------------------------------"
-echo "------ Start Linking Repo Files-------"
-echo "--------------------------------------"
+bar 38 "------ Start Linking Repo Files-------"
 
 # list of files to be linked
 for my_link in .bash_profile .emacs.d .gitconfig .rootrc .inputrc .bash_logout
@@ -57,7 +58,7 @@ do
 	if [ -L $link ] || [ -f $link ] || [ -d $link ]; then
 	    echo -n "exists and "
 	    if [[ $target -ef $link ]]; then
-		echo "already points to ${my_link}"
+                echo -e "${GOOD}already points to ${prog}${NORMAL}"
 		echo -n "${TAB}"
 		ls -lhG --color=auto $link
 		echo "${TAB}skipping..."
@@ -75,16 +76,15 @@ do
 	    echo "does not exist"
 	fi
         # then link
-	echo -n "${TAB}making link... "
-	ln -sv $target $link
+	echo -en "${TAB}${GRH}";hline 72;
+	echo "${TAB}making link... "
+	ln -sv $target $link | sed "s/^/${TAB}/"
+	echo -ne "${TAB}";hline 72;echo -en "${NORMAL}"
     else
-	echo "does not exist"
+        echo -e"${BAD}does not exist${NORMAL}"
     fi
-    echo
 done
-echo "--------------------------------------"
-echo "--------- Done Making Links ----------"
-echo "--------------------------------------"
+bar 38 "--------- Done Making Links ----------"
 
 echo
 sudo ./make_links_etc.sh
