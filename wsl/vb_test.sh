4d098219dc99375152f5b99262541f56e071b013
Jon Lighthall <jon.lighthall@gmail.com>
2023-07-27 16:58:40 -0500

    make vb generic

--- wsl/vb_test.sh
+++ wsl/vb_test.sh
@@ -1,4 +1,6 @@
-echo "VB = '${VB}'"
+VAR=$VB
+
+echo "VAR = '${VAR}'"
 
 TRUE='\x1B[1;32mtrue\x1B[0m'
 FALSE='\x1B[1;31mfalse\x1B[0m'
@@ -7,68 +9,65 @@ UNSET='\x1B[1;33munset\x1B[0m'
 # true false
 echo -e "----------------------------------------------------"
 echo -e -n "             bare : "
-if $VB; then
+if $VAR; then
     echo -e " ${TRUE}"
 else
     echo -e "${FALSE}"
 fi
 echo -e -n "         brackets : "
-if ${VB}; then
+if ${VAR}; then
     echo -e " ${TRUE}"
 else
     echo -e "${FALSE}"
 fi
 
 echo -e -n "        not unset\x1B[0m : "
-if [ ! -z ${VB:+dummy} ]; then
+if [ ! -z ${VAR:+dummy} ]; then
     echo -e " ${TRUE}"
 else
     echo -e "${FALSE}"
 fi
 
 echo -e -n "not unset\x1B[0m and true\x1B[0m: "
-if [ ! -z ${VB:+dummy} ] && ${VB}; then
+if [ ! -z ${VAR:+dummy} ] && ${VAR}; then
     echo -e " ${TRUE}"
 else
     echo -e "${FALSE}"
 fi
-
 
 echo -e -n "      set and true: "
-if [ -n ${VB:+dummy} ] && ${VB}; then
+if [ -n ${VAR:+dummy} ] && ${VAR}; then
     echo -e " ${TRUE}"
 else
     echo -e "${FALSE}"
 fi
-
-
 
 echo -e "----------------------------------------------------"
 # null, no quotes
 echo -e "----------------------------------------------------"
 echo -e -n "    NULL (-z)         : "
-if [ -z ${VB} ]; then
+if [ -z ${VAR} ]; then
     echo -e " ${TRUE}: ${UNSET} or null (empty)"
 else
     echo -e "${FALSE}: set and not null"
 fi
 
 echo -e -n "    NULL (-z -)       : "
-if [ -z ${VB-dummy} ]; then
+if [ -z ${VAR-dummy} ]; then
     echo -e " ${TRUE}: set and null (empty)"
 else
     echo -e "${FALSE}: ${UNSET} or not null"
 fi
 
 echo -e -n "    NULL (-z +)       : "
-if [ -z ${VB+dummy} ]; then
+if [ -z ${VAR+dummy} ]; then
     echo -e " ${TRUE}: ${UNSET}"
 else
     echo -e "${FALSE}: set (maybe null)"
 fi
 
 echo -e -n "    NULL (-z :+)      : "
-if [ -z ${VB:+dummy} ]; then
+if [ -z ${VAR:+dummy} ]; then
     echo -e " ${TRUE}: ${UNSET} or null"
 else
     echo -e "${FALSE}: set and not null"
@@ -77,28 +76,28 @@ fi
 # not null, no quotes
 echo -e "----------------------------------------------------"
 echo -e -n "NOT NULL (! -z)       : "
-if [ ! -z ${VB} ]; then
+if [ ! -z ${VAR} ]; then
     echo -e " ${TRUE}: not null"
 else
     echo -e "${FALSE}: ${UNSET} or null"
 fi
 
 echo -e -n "NOT NULL (! -z -)     : "
-if [ ! -z ${VB-dummy} ]; then
+if [ ! -z ${VAR-dummy} ]; then
     echo -e " ${TRUE}: ${UNSET} or not null"
 else
     echo -e "${FALSE}: set and null"
 fi
 
 echo -e -n "NOT NULL (! -z +)     : "
-if [ ! -z ${VB+dummy} ]; then
+if [ ! -z ${VAR+dummy} ]; then
     echo -e " ${TRUE}: set (maybe null)"
 else
     echo -e "${FALSE}: ${UNSET}"
 fi
 
 echo -e -n "NOT NULL (! -z :+)    : "
-if [ ! -z ${VB:+dummy} ]; then
+if [ ! -z ${VAR:+dummy} ]; then
     echo -e " ${TRUE}: set and not null"
 else
     echo -e "${FALSE}: ${UNSET} or null"
@@ -107,28 +106,28 @@ fi
 # not null, quotes
 echo -e "----------------------------------------------------"
 echo -e -n "NOT NULL (-n \"\")      : "
-if [ -n "${VB}" ]; then
+if [ -n "${VAR}" ]; then
     echo -e " ${TRUE}: set and not null (empty)"
 else
     echo -e "${FALSE}: ${UNSET} or null"
 fi
 
 echo -e -n "NOT NULL (-n - \"\")    : "
-if [ -n "${VB-dummy}" ]; then
+if [ -n "${VAR-dummy}" ]; then
     echo -e " ${TRUE}: ${UNSET} or not null (empty)"
 else
     echo -e "${FALSE}: set and null"
 fi
 
 echo -e -n "NOT NULL (-n + \"\")    : "
-if [ -n "${VB+dummy}" ]; then
+if [ -n "${VAR+dummy}" ]; then
     echo -e " ${TRUE}: set (maybe null)"
 else
     echo -e "${FALSE}: ${UNSET}"
 fi
 
 echo -e -n "NOT NULL (-n :+ \"\")   : "
-if [ -n "${VB:+dummy}" ]; then
+if [ -n "${VAR:+dummy}" ]; then
     echo -e " ${TRUE}: set and not null"
 else
     echo -e "${FALSE}: ${UNSET} or null"
@@ -137,28 +136,28 @@ fi
 # not not null, quotes
 echo -e "----------------------------------------------------"
 echo -e -n "    NULL (! -n \"\")    : "
-if [ ! -n "${VB}" ]; then
+if [ ! -n "${VAR}" ]; then
     echo -e " ${TRUE}: ${UNSET} or null"
 else
     echo -e "${FALSE}: set and not null"
 fi
 
 echo -e -n "    NULL (! -n - \"\")  : "
-if [ ! -n "${VB-dummy}" ]; then
+if [ ! -n "${VAR-dummy}" ]; then
     echo -e " ${TRUE}: set and null"
 else
     echo -e "${FALSE}: ${UNSET} or not null"
 fi
 
 echo -e -n "    NULL (! -n + \"\")  : "
-if [ ! -n "${VB+dummy}" ]; then
+if [ ! -n "${VAR+dummy}" ]; then
     echo -e " ${TRUE}: ${UNSET}"
 else
     echo -e "${FALSE}: set (maybe null)"
 fi
 
 echo -e -n "    NULL (! -n :+ \"\") : "
-if [ ! -n "${VB:+dummy}" ]; then
+if [ ! -n "${VAR:+dummy}" ]; then
     echo -e " ${TRUE}: ${UNSET} or null"
 else
     echo -e "${FALSE}: set and not null"
@@ -167,21 +166,21 @@ echo -e "----------------------------------------------------"
 # not null ands, quotes
 echo -e "----------------------------------------------------"
 echo -e -n "NOT NULL (! -z && -n \"\")    : "
-if [ ! -z ${VB} ] && [ -n "${VB}"  ]; then
+if [ ! -z ${VAR} ] && [ -n "${VAR}"  ]; then
     echo -e " ${TRUE}: set and not null"
 else
     echo -e "${FALSE}: ${UNSET} or null"
 fi
 
 echo -e -n "NOT NULL (! -z && -n + \"\")  : "
-if [ ! -z ${VB+dummy} ] && [ -n "${VB+dummy}"  ]; then
+if [ ! -z ${VAR+dummy} ] && [ -n "${VAR+dummy}"  ]; then
     echo -e " ${TRUE}: set (maybe null)"
 else
     echo -e "${FALSE}: ${UNSET}"
 fi
 
 echo -e -n "NOT NULL (! -z && -n :+ \"\") : "
-if [ ! -z ${VB:+dummy} ] && [ -n "${VB:+dummy}" ]; then
+if [ ! -z ${VAR:+dummy} ] && [ -n "${VAR:+dummy}" ]; then
     echo -e " ${TRUE}: set and not null"
 else
     echo -e "${FALSE}: ${UNSET} or null"
@@ -190,21 +189,21 @@ fi
 # null ands, quotes
 echo -e "----------------------------------------------------"
 echo -e -n "    NULL (-z && ! -n \"\")    : "
-if [ -z ${VB} ] && [ ! -n "${VB}"  ]; then
+if [ -z ${VAR} ] && [ ! -n "${VAR}"  ]; then
     echo -e " ${TRUE}: ${UNSET} or null"
 else
     echo -e "${FALSE}: set and not null"
 fi
    
 echo -e -n "    NULL (-z && ! -n + \"\")  : "
-if [ -z ${VB+dummy} ] && [ ! -n "${VB+dummy}" ]; then
+if [ -z ${VAR+dummy} ] && [ ! -n "${VAR+dummy}" ]; then
     echo -e " ${TRUE}: ${UNSET}"
 else
     echo -e "${FALSE}: set (maybe null)"
 fi
 
 echo -e -n "    NULL (-z && ! -n :+ \"\") : "
-if [ -z ${VB:+dummy} ] && [ ! -n "${VB:+dummy}" ]; then
+if [ -z ${VAR:+dummy} ] && [ ! -n "${VAR:+dummy}" ]; then
     echo -e " ${TRUE}: ${UNSET} or null"
 else
     echo -e "${FALSE}: set and not null"
@@ -214,14 +213,14 @@ echo -e "----------------------------------------------------"
 # impossible and
 echo -e "----------------------------------------------------"
 echo -e -n "    NULL and NOT NULL (-z && -n \"\")     : "
-if [ -z ${VB} ] && [ -n "${VB}"  ]; then
+if [ -z ${VAR} ] && [ -n "${VAR}"  ]; then
     echo -e " ${TRUE}: impossible!"
 else
     echo -e "${FALSE}: OK"
 fi
 
 echo -e -n "NOT NULL and     NULL (! -z && ! -n \"\") : "
-if [ ! -z ${VB} ] && [ ! -n "${VB}"  ]; then
+if [ ! -z ${VAR} ] && [ ! -n "${VAR}"  ]; then
     echo -e " ${TRUE}: impossible!"
 else
     echo -e "${FALSE}: OK"
@@ -230,25 +229,25 @@ echo -e "----------------------------------------------------"
 
 return
 echo -e -n "NULL (-z :+): "
-if [ -z ${VB:+dummy} ]; then
+if [ -z ${VAR:+dummy} ]; then
     echo -e "${UNSET}"
 else
     echo -e "set"
-    if [ -z ${VB} ]; then
-	echo -e " ${TRUE}: VB -z yes"
+    if [ -z ${VAR} ]; then
+	echo -e " ${TRUE}: VAR -z yes"
     else
-	echo -e "${FALSE}: VB -z no"
+	echo -e "${FALSE}: VAR -z no"
     fi
 
-    if [ -n ${VB} ]; then
-	echo -e " ${TRUE}: VB -n yes"
+    if [ -n ${VAR} ]; then
+	echo -e " ${TRUE}: VAR -n yes"
     else
-	echo -e "${FALSE}: VB -n no"
+	echo -e "${FALSE}: VAR -n no"
     fi
-    if ${VB}; then
-	echo -e " ${TRUE}: VB yes"
+    if ${VAR}; then
+	echo -e " ${TRUE}: VAR yes"
     else
-	echo -e "${FALSE}: VB no"
+	echo -e "${FALSE}: VAR no"
     fi
 
 fi
