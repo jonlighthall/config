248eadd07556b653b2ce7fbb64b7699dd87c2293
Jon Lighthall <jon.lighthall@gmail.com>
2023-08-09 17:37:41 -0500

    fix typo, comment delete trailing

--- emacs_all.el
+++ emacs_all.el
@@ -39,7 +39,7 @@
   (interactive "*")
   (push-mark) ; save position
   (indent-region (point-min) (point-max)) ; select all
-  (delete-trailing-whitespace)
+  ;;  (delete-trailing-whitespace)
   (goto-char 1)
   (replace-regexp "^\n\\{2,\\}" "\n") ; delete repeated blank lines
   (pop-mark) ; pop mark position saved by regexp
@@ -149,28 +149,35 @@
 (add-to-list 'auto-mode-alist '("\\.m\\'" . octave-mode))
 
 ;; MATLAB tabbing
-(add-hook 'octave-mode-hook (lambda ()
-			      (setq indent-tabs-mode t)
-			      (setq tab-stop-list (number-sequence matb 200 mtab))
-			      (setq tab-width mtab)
-			      (setq indent-line-function 'insert-tab) ))
+(add-hook 'octave-mode-hook
+	  (lambda ()
+	    (setq indent-tabs-mode t)
+	    (setq tab-stop-list (number-sequence mtab 200 mtab))
+	    (setq tab-width mtab)
+	    (setq indent-line-function 'insert-tab)
+	    )
+	  )
 
 (setq octave-mode-hook
-      (lambda () (progn (setq octave-comment-char ?%)
-                        (setq comment-start "%")
-                        (setq indent-tabs-mode t)
-                        (setq comment-add 0)
-                        (setq tab-width mtab)
-                        (setq tab-stop-list (number-sequence mtab 200 mtab))
-                        (setq octave-block-offset mtab)
+      (lambda () (progn
+		   (setq octave-comment-char ?%)
+                   (setq comment-start "%")
+                   (setq indent-tabs-mode t)
+                   (setq comment-add 0)
+                   (setq tab-width mtab)
+                   (setq tab-stop-list (number-sequence mtab 200 mtab))
+                   (setq octave-block-offset mtab)
 
-                        (defun octave-indent-comment ()
-                          "A function for `smie-indent-functions' (which see)."
-                          (save-excursion
-                            (back-to-indentation)
-                            (cond
-                             ((octave-in-string-or-comment-p) nil)
-                             ((looking-at-p "\\(\\s<\\)\\1\\{2,\\}") 0)))))))
+                   (defun octave-indent-comment ()
+                     "A function for `smie-indent-functions' (which see)."
+                     (save-excursion
+                       (back-to-indentation)
+                       (cond
+                        ((octave-in-string-or-comment-p) nil)
+                        ((looking-at-p "\\(\\s<\\)\\1\\{2,\\}") 0))))
+		   )
+	)
+      )
 
 ;; open arduino as c++
 (add-to-list 'auto-mode-alist '("\\.ino\\'" . c++-mode))
