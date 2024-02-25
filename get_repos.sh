42b2d6f16a8184cf0bff17a0266dba8892fd762c
Jon Lighthall <jon.lighthall@gmail.com>
2024-02-25 15:22:45 -0600

    add get distro name, lint

--- get_repos.sh
+++ get_repos.sh
@@ -4,36 +4,36 @@
 # load formatting
 fpretty=${HOME}/utils/bash/.bashrc_pretty
 if [ -e $fpretty ]; then
-    source $fpretty
+	source $fpretty
 fi
 
 # determine if sourcing or executing
 if (return 0 2>/dev/null); then
-    RUN_TYPE="sourcing"
+	RUN_TYPE="sourcing"
 else
-    RUN_TYPE="executing"
-    # exit on errors
-    set -e
+	RUN_TYPE="executing"
+	# exit on errors
+	set -e
 fi
 # print source name at start
 echo -e "${TAB}${RUN_TYPE} ${PSDIR}$BASH_SOURCE${NORMAL}..."
 src_name=$(readlink -f $BASH_SOURCE)
 if [ ! "$BASH_SOURCE" = "$src_name" ]; then
-    echo -e "${TAB}${VALID}link${NORMAL} -> $src_name"
+	echo -e "${TAB}${VALID}link${NORMAL} -> $src_name"
 fi
 # set tab
 TAB='   '
 if [ $# -eq 0 ]; then
-    echo "No system specified"
+	echo "No system specified"
 fi
 # set file name to be run in system directory
 fname=make_links.sh
 # check if configuration is specified
 if [ $# -eq 1 ]; then
-    echo "Loading configuration options for $1"
-    echo -n "${TAB}$1... "
+	echo "Loading configuration options for $1"
+	echo -n "${TAB}$1... "
 	# check if directory exists
-    if [ -d ${HOME}/config/$1 ]; then
+	if [ -d ${HOME}/config/$1 ]; then
 		echo "found"
 		cd ${HOME}/config/$1
 		echo -n "${TAB}$fname... "
@@ -57,45 +57,60 @@ if [ $# -eq 1 ]; then
 		else
 			echo "not found"
 		fi
-    else
+	else
 		echo "not found"
-    fi
+	fi
 fi
 
+# get distro name
+if [ -f /etc/os-release ]; then
+	distro=$(sed '/^NAME/!d' /etc/os-release | awk -F \" '{print $2}')
+else
+	if command -v lsb_release; then
+		distro=$(lsb_release -a 2>&1 | sed '/Distributor/!d;s/^.*:[ \t]*//')
+	else
+		sed '/^NAME/!d' /etc/*release | awk -F\" '{print $2}'
+	fi
+fi
+
+# convert to lower case
+distro=$(echo "$distro" | tr '[:upper:]' '[:lower:]')
+echo "distro: $distro"
+
+exit
+
 # check if git is defined
-if  command -v git ; then
-    echo "proceeding with Git commands..."
+if command -v git; then
+	echo "proceeding with Git commands..."
 
-    # define directory names
-    rdir=${HOME}/repos
-    udir=${HOME}/utils
-    edir=${HOME}/examp
+	# define directory names
+	rdir=${HOME}/repos
+	udir=${HOME}/utils
+	edir=${HOME}/examp
 
-    echo "creating repository directories..."
-    for my_dir in $rdir $udir $edir
-    do
+	echo "creating repository directories..."
+	for my_dir in $rdir $udir $edir; do
 		if [ ! -d ${my_dir} ]; then
 			mkdir -vp ${my_dir}
 		else
 			echo "${TAB}directory ${my_dir} already exists"
 		fi
-    done
+	done
 
-    echo "--------------------------------------"
-    echo "------ Start Cloning Repo Files-------"
-    echo "--------------------------------------"
+	echo "--------------------------------------"
+	echo "------ Start Cloning Repo Files-------"
+	echo "--------------------------------------"
 
-    github_user=jonlighthall
-    github_https=https://github.com/${github_user}/
-    github_ssh=git@github.com:${github_user}/
-    github_auth=${github_ssh}
+	github_user=jonlighthall
+	github_https=https://github.com/${github_user}/
+	github_ssh=git@github.com:${github_user}/
+	github_auth=${github_ssh}
 
-    cd ${rdir}
-    # list of utility repos to be cloned
+	cd ${rdir}
+	# list of utility repos to be cloned
 	gname="utility"
 	echo "cloning ${gname} repos..."
-    for my_repo in bash batch fortran_utilities powershell
-    do
+	for my_repo in bash batch fortran_utilities powershell; do
 		if [ ! -d ${my_repo} ]; then
 			echo "cloning $my_repo..."
 			git clone ${github_auth}${my_repo}.git
@@ -113,29 +128,28 @@ if  command -v git ; then
 
 		# run make_links
 		fname="${link}/make_links.sh"
-		if [ -e "${fname}" ];then
+		if [ -e "${fname}" ]; then
 			cd ${link}
 			bash ${fname}
 			cd ${rdir}
 		fi
-    done
+	done
 	echo -e "done cloning ${gname} repos"
 
-    # load formatting
-    if [ -e $fpretty ]; then
+	# load formatting
+	if [ -e $fpretty ]; then
 		source $fpretty
 		cbar "${magenta}pretty print enabled${NORMAL}"
-    fi
+	fi
 
 	# reset TAB
 	TAB=''
 
-    # list of example repos to be cloned
+	# list of example repos to be cloned
 	gname="example"
 	echo "cloning ${gname} repos..."
 	TAB+=${fTAB:='   '}
-    for my_repo in cpp fortran hello nrf python
-    do
+	for my_repo in cpp fortran hello nrf python; do
 
 		# define target (source)
 		target=${rdir}/${my_repo}
@@ -160,7 +174,7 @@ if  command -v git ; then
 			TAB+=${fTAB}
 			echo -n "exits and "
 			if [[ "${target}" -ef ${link} ]]; then
-                echo "already points to ${my_repo}"
+				echo "already points to ${my_repo}"
 				echo -n "${TAB}"
 				ls -lhG --color=auto ${link}
 				echo "${TAB}skipping..."
@@ -184,41 +198,42 @@ if  command -v git ; then
 		else
 			echo "does not exist"
 		fi
-        # then link
-		echo -en "${TAB}${GRH}";hline 72;
+		# then link
+		echo -en "${TAB}${GRH}"
+		hline 72
 		echo "${TAB}making link... "
 		ln -sv "${target}" ${link} | sed "s/^/${TAB}/"
-		echo -ne "${TAB}";hline 72;echo -en "${NORMAL}"
+		echo -ne "${TAB}"
+		hline 72
+		echo -en "${NORMAL}"
 		TAB=${TAB%$fTAB}
-    done
+	done
 	TAB=${TAB%$fTAB}
 	echo -e "done cloning ${gname} repos"
 
-    # list of other repos to be cloned
+	# list of other repos to be cloned
 	gname="other"
 	echo "cloning ${gname} repos..."
 	TAB+=${fTAB:='   '}
-	for my_repo in matlab
-    do
+	for my_repo in matlab; do
 		if [ ! -d ${my_repo} ]; then
 			echo "cloning $my_repo..."
 			git clone ${github_auth}$my_repo
 		else
 			echo -e "${TAB}dirctory ${yellow}$PWD/$my_repo${NORMAL} already exits"
 		fi
-    done
+	done
 	TAB=${TAB%$fTAB}
 	echo -e "done cloning ${gname} repos"
 
-    # list of private repos to be cloned
-    if [[ ! ("$(hostname -f)"  == *"navy.mil") ]]; then
+	# list of private repos to be cloned
+	if [[ ! ("$(hostname -f)" == *"navy.mil") ]]; then
 		gname="private"
 		echo "cloning ${gname} repos..."
 		TAB+=$fTAB
 		cd ${HOME}/config
 		dname=private
-		for my_repo in config_private
-		do
+		for my_repo in config_private; do
 			if [ ! -d $dname ]; then
 				echo "cloning $dname..."
 				echo "see ${github_https}$my_repo/blob/master/.git-credentials"
@@ -241,8 +256,8 @@ if  command -v git ; then
 		done
 		TAB=${TAB%$fTAB}
 		echo -e "done cloning ${gname} repos"
-    fi
+	fi
 	echo "done cloning all repos"
 else
-    echo "Git not defined."
+	echo "Git not defined."
 fi
