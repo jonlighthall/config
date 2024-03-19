3fd3e25212b27bc87d34dd4c1f570ea0f4e3dd1d
Jon Lighthall <jon.lighthall@gmail.com>
2024-03-19 12:41:30 -0500

    add get cursor position

--- lib_fmt.sh
+++ lib_fmt.sh
@@ -1,17 +1,80 @@
+#!/bin/bash -u
+
+function get_curpos() {
+    # Turn in-function debugging on/off.
+    # Inherit the value of funcDEBUG from shell or substitute default value if unset or NULL.
+    local -i funcDEBUG=${funcDEBUG:-1}
+    local -i DEBUG=${DEBUG+1}    
+    local CURPOS
+    # get the cursor position
+    echo -en "\E[6n"
+    # print response
+    if [ $DEBUG -eq 0 ]; then
+        read -sdR CURPOS
+    else
+        decho -n "cursor text: '"        
+        read -dR CURPOS
+        decho  "'"
+    fi
+
+	  # parse the cursor position
+	  local -r pair=${CURPOS#*[}
+          #}# dummy bracket for emacs indenting
+	  local -ir x_pos=${pair#*;}
+	  local -ir y_pos=${pair%;*}
+
+	  # print the cursor position
+	  fecho -e "cursor position: x=$x_pos y=$y_pos"
+
+    fecho -n "x-position is $x_pos: "
+    if [ ${x_pos} -gt 1 ]; then
+        fecho -n "new line"
+        printf '\n'
+    else
+        fecho "stay here"
+    fi
+
+    fecho "number of args: $#"
+    
+    # output values to parent   
+    if [ $# -gt 0 ]; then
+        fecho "outputing..."
+        local -n x_out=$1
+        fecho "   arg 1 in : ${!x_out}=$x_out"
+        fecho "   arg 1 out: ${!x_out}=${x_pos}"
+        x_out=$x_pos        
+    fi
+
+    if [ $# -gt 1 ]; then
+        local -n y_out=$2
+        fecho "   arg 2 in : ${!y_out}=$y_out"
+        fecho "   arg 2 out: ${!y_out}=${y_pos}"
+        y_out=$y_pos        
+    fi
+}
+
+function ind() {
+    echo -n "hello "
+    local -i x=0
+    local -i y=0
+    # pass variable names, not values
+    get_curpos x y
+    echo "x = $x"
+    echo "y = $y"
+}
+
 # start a new line only if not already on a new line
 # i.e., carriage return with conditional line feed
 function start_new_line() {
     # get the cursor position
-    echo -en "\E[6n"
-    read -sdR CURPOS
-    local CURPOS=${CURPOS#*[}
-          #}# dummy bracket for emacs indenting
-    # get the x-position of the cursor
-    local -i x_pos=${CURPOS#*;}
+    local -i x
+    get_curpos x
     # if the cursor is not at the start of a line, then create a new line
-    if [ ${x_pos} -gt 1 ]; then
-        printf '\e[0;90;40m\u21b5\n\e[m'
+    if [ ${x} -gt 1 ]; then
+        printf '\e[0;90;40mNEW LINE\u21b5\n\e[m'
         #echo
+    else
+        echo -en "\x1b[7mNO NEW LINE\x1b[m"
     fi
 }
 
