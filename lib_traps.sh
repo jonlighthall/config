71186de9d665785c43a6227dbf08ca8cc4659c30
Jon Lighthall <jon.lighthall@gmail.com>
2024-03-21 12:18:29 -0500

    add handling for bash errors in lib traps

--- lib_traps.sh
+++ lib_traps.sh
@@ -76,7 +76,6 @@ function reset_shell() {
             decho
             ddecho "fixing $opt"
         fi
-
         
         # strip + from option
         if [[ "${opt::1}" == "+" ]]; then                
@@ -123,46 +122,12 @@ function huh() {
 }
 
 # print function stack
-function print_stack() {
-    start_new_line
+function print_stack_devel() {    
     local -ir DEBUG=2
-
+    print_stack
+    
     echo "BASH_COMMAND = $BASH_COMMAND"
-    echo "BASH_SUBSHELL = $BASH_SUBSHELL"
-    
-    # get length of function stack
-    local -ir N_FUNC=${#FUNCNAME[@]}
-    local -ir N_BASH=${#BASH_SOURCE[@]}
-    local -ir N_LINE=${#BASH_LINENO[@]}
-
-    # print length of stack
-    echo "${TAB}There are N=$N_FUNC entries in the execution call stack"
-    
-    # check that all stacks have the same length
-    if [ $N_FUNC -ne $N_BASH ]; then 
-        echo "${TAB}There are N=$N_BASH entries in the source file name stack"
-    fi
-
-    if [ $N_FUNC -ne $N_LINE ]; then 
-        echo "${TAB}There are N=$N_LINE entries in the line number stack"
-    fi
-
-    if [ $N_FUNC -ne $N_BASH ] || [ $N_FUNC -ne $N_LINE ]; then 
-        echo "${TAB}${N_FUNC} functions, ${N_BASH} files, ${N_LINE]} lines"
-    fi
-
-    # print call stack
-    echo "${TAB}call stack:"
-    local -i i
-    for ((i = 0; i < $N_FUNC ; i++)); do
-        echo "$i ${FUNCNAME[i]} ${BASH_SOURCE[i]} ${BASH_LINENO[i]}"
-    done
-    return 0
-
-    # resolve symbolic links
-    for ((i = 0; i < $N; i++)); do
-        BASH_LINK[$i]=$(readlink -f ${BASH_SOURCE[$i]})
-    done
+    echo "BASH_SUBSHELL = $BASH_SUBSHELL"    
     
     echo "${TAB}list of sources:"
     (
@@ -293,12 +258,20 @@ function print_stack() {
 
 }
 
-function print_stack2() {
+function print_stack() {
+    local -i DEBUG=9
+    start_new_line
     # get length of function stack
     local -gi N_FUNC=${#FUNCNAME[@]}
     local -gi N_BASH=${#BASH_SOURCE[@]}
     local -gi N_LINE=${#BASH_LINENO[@]}
 
+    # get color index
+    local -i idx
+    dbg2idx $N_BASH idx
+    # set color
+    echo -ne "${dcolor[idx]}"
+    
     # print length of stack
     echo "${TAB}There are N=$N_FUNC entries in the execution call stack"
     
@@ -337,18 +310,13 @@ function print_stack2() {
         done
     ) | column -t -s: -N "index,function,source,line no" -R1 | sed "s/^/${TAB}/"
     dtab
+    # unset color
+    echo -ne "\e[0m"
 }
 
 function print_invo() {
-    local DEBUG=1
-
-    # print invoking source or function
-    decho "${TAB}BASH_SOURCE=${BASH_SOURCE[@]}"
-    decho "${TAB}FUNCNAME=${FUNCNAME[@]}"
-    decho "${TAB}BASH_LINENO=${BASH_LINENO[@]}"
-
-    print_stack2
-
+    local DEBUG=9
+    print_stack
     # since print stack is itself part of the stack, remove the top of the stack
     ((N_FUNC--))
     ((N_BASH--))    
@@ -492,6 +460,9 @@ function print_error() {
     # e.g.
     # trap 'print_error $LINENO $? $BASH_COMMAND' ERR
 
+    local -i DEBUG=9
+    local -i funcDEBUG=9
+    
     # parse arguments
     local -i ERR_LINENO=$1
     shift
@@ -499,77 +470,145 @@ function print_error() {
     shift
     local ERR_CMD="$@"
 
+    fecho "LINENO = $ERR_LINENO"
+    fecho "RETVAL = $ERR_RETVAL"
+    fecho "   CMD = $ERR_CMD"
+
+    print_stack
+
+    
+    # since print stack is itself part of the stack, remove the top of the stack
+    ((N_FUNC--))
+    ((N_BASH--))    
+    ((N_LINE--))
+
+    # get the bottom of the stack: the call stack counts from zero
+    local -i N_BOTTOM=$(($N_FUNC - 1))
+    decho "${TAB}getting bottom of stack..."
+    itab
+    ddecho "${TAB}N_BOTTOM = $N_BOTTOM"
+    ddecho "${TAB}FUNCNAME[$N_BOTTOM] = ${FUNCNAME[$N_BOTTOM]}"
+    ddecho "${TAB}BASH_SOURCE[$N_BOTTOM] = ${BASH_SOURCE[$N_BOTTOM]}"
+    if [[ ${BASH_SOURCE[${N_BOTTOM}]} =~ "bash" ]]; then
+        ddecho "${TAB}bottom of stack is bash"
+    else
+        ddecho "${TAB}bottom of stack is NOT bash"
+    fi
+    dtab
+
+    # get the command that casued the error
+    decho "${TAB}getting line that caused error..."
+    itab
+
+    local ERR_LINE=
+
+    if [[ ${BASH_SOURCE[${N_BOTTOM}]} =~ "bash" ]]; then
+        # bash
+        ddecho "${TAB}error did not come from a file, it came from bash"
+        local ERR_LINE=$ERR_CMD
+    else
+        # not bash
+        ddecho "${TAB}base of stack is not bash"
+        # check that error came from a file        
+        if [ -f "${BASH_SOURCE[1]}" ]; then
+            ddecho "${TAB}BASH_SOURCE[1] is a file"
+            # check that value is not empty
+            if [ ! -z "${BASH_SOURCE[1]}" ]; then
+                ddecho "${TAB}BASH_SOURCE[1] is not empty"
+                if [ $N_FUNC -gt 1 ] ; then
+                    ddecho "${TAB}BASH_SOURCE[1] = ${BASH_SOURCE[1]}"
+                    ddecho "${TAB}there are functions in stack"
+                    ddecho "${TAB}ERR_LINE = ${ERR_LINENO}"
+
+                    ddecho -n "${TAB}line ${ERR_LINENO} in ${BASH_SOURCE[1]}: "
+                    ddecho sed -n "${ERR_LINENO}p" "${BASH_SOURCE[1]}"
+                    decho "${TAB}"$(sed -n "${ERR_LINENO}p" "${BASH_SOURCE[1]}")
+                    
+                    ERR_LINE=$(sed -n "${ERR_LINENO}p" "${BASH_SOURCE[1]}" | sed "s/^\s*//")
+                else
+                    ERR_LINE="???"
+                fi
+            fi
+        else
+            ddecho "${TAB}error did not come from a file"
+            
+            ERR_LINE="$ERR_CMD"
+        fi
+
+        
+    fi
+    dtab
+    
     # print summary
     start_new_line
     local ERR_PRINT=$(echo -e "${TAB}\E[37;41m ERROR ${RESET} ")
     echo -n ${ERR_PRINT}
-    # print grep-like line match
-    echo -ne " \E[35m${BASH_SOURCE[1]##*/}\E[m\E[36m:\E[m\E[32m${ERR_LINENO}\E[m\E[36m:\E[m"
-
-    # get the cursor position
-    echo -en "\E[6n"
-    read -sdR CURPOS
-    local CURPOS=${CURPOS#*[}
-          #}# dummy bracket for Emacs indenting
-    # get the x-position of the cursor
-    local -i x_pos=${CURPOS#*;}
-    ((--x_pos))
-    ((--x_pos))
-    # define indent
-    local spx=$(echo -ne "\E[${x_pos}C")
-
-    print_stack
-
-    # get length of function stack
-    local -ir N_FUNC=${#FUNCNAME[@]}
-
-    if [ -f "${BASH_SOURCE[1]}" ]; then
-        echo "${BASH_SOURCE[1]} is a file"
-        if [ ! -z "${BASH_SOURCE[1]}" ]; then
-            echo "${BASH_SOURCE[1]} variable is not empty"
-            if [ $N_FUNC -gt 1 ] ; then
-                echo "there are functions in stack"
-                local ERR_LINE
-                
-                echo "${ERR_LINENO}p"
-                echo "${BASH_SOURCE[1]}"
-                echo ${BASH_SOURCE[1]}
-                echo sed -n "${ERR_LINENO}p" "${BASH_SOURCE[1]}"
-                sed -n "${ERR_LINENO}p" "${BASH_SOURCE[1]}"
-                
-                ERR_LINE=$(sed -n "${ERR_LINENO}p" "${BASH_SOURCE[1]}" | sed "s/^\s*//") && echo "OK"
-            fi
+    
+    # print grep-like line match    
+    if [[ ${BASH_SOURCE[${N_BOTTOM}]} =~ "bash" ]]; then
+        echo -ne " ${GRF}${FUNCNAME[N_BOTTOM]}${RESET}${GRS}:${RESET} "
+    else
+        if [ ${N_BOTTOM} -eq 0 ]; then
+            echo -ne " ${GRF}${SHELL}${RESET}${GRS}:${RESET}${GRL}${ERR_LINENO}${RESET}${GRS}:${RESET}"
+        else
+            echo -ne " ${GRF}${BASH_SOURCE[1]##*/}${RESET}${GRS}:${RESET}${GRL}${ERR_LINENO}${RESET}${GRS}:${RESET}"
         fi
     fi
-    
-    echo "${ERR_LINE}"
+
+    # get the cursor position
+    local -i x_pos
+    get_curpos x_pos
+    # define indent
+    ((--x_pos))
+    ((--x_pos))
+    local spx=$(echo -ne "\E[${x_pos}C")
     eva='eval'
     local -i evl=${#eva}
     local -i etab=$((x_pos - evl))
 
-    if [[ "$ERR_CMD" != "$ERR_LINE" ]]; then
-        echo -ne "\E[${etab}C${YELLOW} cmd\E[36m:\E[m"
-        echo "${ERR_CMD}"
+    # print error line
+    echo -n "${ERR_LINE}"
+      if [ ${N_BOTTOM} -eq 0 ]; then
+          ddecho -e " <- ${INVERT}you did this"
+      else
+          echo
     fi
 
-    # if command contains variables, evaluate expression
-    if [[ "$ERR_CMD" =~ '$' ]]; then
-        decho -e "${spx} ${GRAY}expanding variables...${RESET}"
-        # print 'eval' set back from cmd
-        echo -ne "\E[${etab}C${VALID}${eva}\E[m\E[36m:\E[m"
-        # print evaluated command and remove leading whitespace
-        eval echo $ERR_CMD | sed "s/^\s*//"
-        if [[ "$ERR_CMD" =~ "/dev/null" ]]; then
-            # remove redirect
-            NCMD=$(echo $ERR_CMD | sed 's|/dev/null||')
-            NCMD=$(echo $NCMD | sed 's/[12>&\s]*$//')
-            # print evaluated output
-            echo -en "\E[${spx}C"
-            eval echo $NCMD
-            echo -e "$ {GRAY}redirect (no output)${RESET}"
+    # if the error line and the error command do not mach, print error command
+    if [[ "$ERR_CMD" != "$ERR_LINE" ]]; then
+        echo -ne "\E[${etab}C${YELLOW} cmd${GRS}:${RESET}"
+        echo "${ERR_CMD}"
+    fi    
+    
+    if [[ ${BASH_SOURCE[${N_BOTTOM}]} =~ "bash" ]]; then
+        # bash
+        :
+    else
+        # if command contains variables, evaluate expression
+        if [[ "$ERR_CMD" =~ '$' ]]; then
+            decho -e "${spx} ${GRAY}expanding variables...${RESET}"
+            # print 'eval' set back from cmd
+            echo -ne "\E[${etab}C${VALID}${eva}${RESET}${GRS}:${RESET}"
+            # print evaluated command and remove leading whitespace
+            eval echo $ERR_CMD | sed "s/^\s*//"
+            if [[ "$ERR_CMD" =~ "/dev/null" ]]; then
+                # remove redirect
+                NCMD=$(echo $ERR_CMD | sed 's|/dev/null||')
+                NCMD=$(echo $NCMD | sed 's/[12>&\s]*$//')
+                # print evaluated output
+                echo -en "\E[${spx}C"
+                eval echo $NCMD
+                echo -e "$ {GRAY}redirect (no output)${RESET}"
+            fi
         fi
     fi
     echo -e "${spx} ${GRAY}RETVAL=${ERR_RETVAL}${RESET}"
+
+  
+    
+    return 0   
+
+    
 }
 
 function print_int() {
