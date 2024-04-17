54f6deea54525d6aae08565fdda9f0adb709bedd
Jon Lighthall <jon.lighthall@gmail.com>
2024-04-17 10:36:11 -0500

    unify install packages with update packages

--- wsl/install_packages.sh
+++ wsl/install_packages.sh
@@ -1,10 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-# update and upgrade
+# load formatting
+fpretty=${HOME}/config/.bashrc_pretty
+if [ -e $fpretty ]; then
+    source $fpretty    
+fi
+
+# update
+bar "update..."
 sudo apt update
+
+# upgrade
+bar "upgrade and fix missing..."
 sudo apt upgrade -y --fix-missing
 
-# install packages
+# install
+bar "install packages..."
 sudo apt install -y git
 sudo apt install -y nautilus
 
@@ -18,9 +29,13 @@ sudo apt install -y x11-apps
 # see github.com/jonlighthall/bash for X11 test
 
 # re-check
+bar "upgrade and fix missing..."
 sudo apt upgrade -y --fix-missing
 
 # cleanup
+bar "autoremove and purge..."
 sudo apt autoremove --purge -y
+bar "autoclean..."
 sudo apt autoclean
+bar "clean..."
 sudo apt clean
