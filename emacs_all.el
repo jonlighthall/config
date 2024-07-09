99d5127848c6e0c47d5c989a750874c04ac571aa
Jon Lighthall <jonathan.c.lighthall@us.navy.mil>
2024-07-09 08:28:56 -0500

    unify custom-set comments

--- emacs_all.el
+++ emacs_all.el
@@ -29,6 +29,8 @@
 (setq-default indent-tabs-mode nil)
 
 (custom-set-variables
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(ediff-split-window-function (quote split-window-horizontally))
  '(fortran-continuation-string ">")
  '(fortran-line-length fcols)
@@ -279,8 +281,6 @@
   (add-to-list 'default-frame-alist '(background-color . "#FFF")) ; white
 
   (custom-set-faces
-   ;; custom-set-faces was added by Custom.
-   ;; If you edit it by hand, you could mess it up, so be careful.
    ;; Your init file should contain only one such instance.
    ;; If there is more than one, they won't work right.
    '(minibuffer-prompt ((t (:background "#FFF"  :foreground "medium blue")))))
