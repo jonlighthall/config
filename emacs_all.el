65560fb7de4d4820065dae9f2e76b468a23c002a
Jon Lighthall <jon.lighthall@gmail.com>
2025-10-20 09:30:04 -0500

    add version check for fill column indicator

--- emacs_all.el
+++ emacs_all.el
@@ -18,8 +18,10 @@
 ;; Set line width for fortran and other
 (set 'fcols 72)
 (set 'ncols 80)
-(add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
-(set-face-foreground 'fill-column-indicator "color-253")
+;; display-fill-column-indicator-mode is only available in Emacs 27.1+
+(when (>= emacs-major-version 27)
+  (add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
+  (set-face-foreground 'fill-column-indicator "color-253"))
 
 ;; Tab settings
 ;; MATLAB tab size
@@ -297,5 +299,4 @@
             'turn-on-auto-fill
             (setq-local whitespace-line-column fcols)
             (setq-local global-whitespace-mode 1)
-            (setq fortran-line-number-indent 5)
-            (setq fortran-electric-line-number t)))
+            (setq fortran-line-number-indent 5)))
